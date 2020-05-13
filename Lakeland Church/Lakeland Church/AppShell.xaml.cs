8d949b154aae1e1648d511a49634fa79c654fe3a
Toby Samples <toby.samples@gmail.com>
2020-05-13 14:56:45 -0500

    first commit

---
+++ Lakeland Church/Lakeland Church/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Lakeland_Church
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
